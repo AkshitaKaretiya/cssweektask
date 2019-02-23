c0c9f633ec5c9cc2b33d6cd2a529bc1e71c215cd
Akshita_Karetiya <karetiyaakshita@gmail.com>
2019-02-23 18:33:55 +0530

    css weekend task

---
+++ weekendTask/javascript/custom.js
@@ -0,0 +1,29 @@
+// $(document).ready(function(){
+// 	$('[data-toggle="tooltip"]').tooltip();
+// 	$('.search_btn').click(function(){
+// 		var input_value = document.getElementById("search").value;
+//     console.log(input_value);
+// 		if(input_value.length > 0){
+// 			$('.success-alt').toggle();
+// 		}else{
+// 			$('.danger-alt').toggle();
+// 		}
+// 	});
+// });
+
+
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+// window.onclick = function(event) {
+//   if (!event.target.matches('.drop_down')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// };
